bc88f52358265aa1f4dd5bcca9e8a90e1f967a35
ShinBG <shinws9090@naver.com>
2021-05-06 14:19:30 +0900

    autowired 초기작업

---
+++ src/main/java/spring5_autoeired_study/spring/ChangePasswordService.java
@@ -0,0 +1,23 @@
+package spring5_autoeired_study.spring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import spring5_autoeired_study.exception.MemberNotFoundException;
+
+public class ChangePasswordService {
+	@Autowired
+	private MemberDao memberDao; //연관관계 ,메소드에 매계변수로만 있으면 의존관계
+
+	
+	public void changePassword(String email, String oldPwd, String newPwd) {
+		Member member = memberDao.selectByEmail(email);
+		if(member == null) {
+			throw new MemberNotFoundException();
+		}
+		
+		member.changePassword(oldPwd, newPwd);
+		memberDao.update(member);
+	}
+	
+	
+}
